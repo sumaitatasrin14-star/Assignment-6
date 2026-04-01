90a65a373ae9c1a3e463fa160dbb330c8c47ff25
sumaitatasrin14-star <sumaita.tasrin14@gmail.com>
2026-04-01 22:45:34 +0600

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import NavBar from './Component/NavBar'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+          <NavBar></NavBar>
+          <hr className='bg-gray-500'></hr>
+          
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,39 @@
+import ShoppingImg from "../assets/products/shopping-cart.png";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-start">
+        <div className="flex items-center gap-1 font-bold text-xl">
+          DigiTools
+        </div>
+      </div>
+      <div className="navbar-center hidden md:flex">
+        <ul className="menu menu-horizontal px-1 text-lg mx-auto">
+           <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testemonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-1">
+        <div className="btn"> <img src={ShoppingImg}></img></div>
+        <a className="btn">Log in</a>
+        <a className="btn bg-purple-500 rounded-full text-white">Get Started</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
